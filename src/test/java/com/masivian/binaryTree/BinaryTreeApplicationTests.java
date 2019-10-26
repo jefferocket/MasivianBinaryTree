5039cbacdf2312bcc9516c1e1c106532d3561d26
Jeferson Rincón <jeff@jeff.local>
2019-10-26 15:31:26 -0500

    Setting up project

---
+++ src/test/java/com/masivian/binaryTree/BinaryTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masivian.binaryTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
